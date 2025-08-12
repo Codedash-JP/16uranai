b3b79a9a6b5f7291c969407886faf88a995b9583
Codedash-JP <quantdash+coder@gmail.com>
2025-08-13 07:21:59 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // Update "site" to your actual Pages URL after first deploy if needed.
 export default defineConfig({
-  site: 'https://YOUR_USER.github.io/16p-astro',
+  site: 'https://Codedash-JP.github.io/16p-astro',
   base: '/16p-astro',
 });
